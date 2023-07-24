6e66b90f5189c6e947ce8429b0e8733af5f374b6
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-07-24 22:17:52 +0300

    iesRotor fix

--- iesRotor.m
+++ iesRotor.m
@@ -3,10 +3,12 @@
 
 function f = iesRotor(ies, rot)
 I = ies.I;
+stepA = 360/(length(I(:,1))-1);
 Ii(1:length(I(:,1)), 1:length(I(1,:))) = 0;
-Ii(1:rot,:) = I(end-(rot-1):end,:);
-Ii(rot+1:end,:) = I(1:end-rot,:);
+Ii(1:floor(rot/stepA),:) = I(end-(floor(rot/stepA)-1):end,:);
+Ii(floor(rot/stepA)+1:end,:) = I(1:end-floor(rot/stepA),:);
 iesResult = ies;
 iesResult.I = Ii;
+disp(['КСС повернута на ' num2str(rot) ' градусов']);
 f = iesResult;
 end
